e55e51ce3afbec08c7c58d2827bb4e6f7a367509
n14s <n14x@posteo.de>
2022-03-27 10:46:06 -0500

    add wait for aas-server; overwrite 1.0.1

--- script/docker-entrypoint.sh
+++ script/docker-entrypoint.sh
@@ -3,12 +3,18 @@
 # Abort on any error (including if wait-for-it fails).
 set -e
 
-# Wait for the backend to be up, if we know where it is.
+# Wait for prometheus to be up, if we know where it is.
 if [ -n "$PROMETHEUS_HOST" ]; then
   /app/wait-for-it.sh "$PROMETHEUS_HOST:${PROMETHEUS_PORT:-9090}"
 fi
 
-sleep 3
+# Wait for the aas-server to be up, if we know where it is.
+if [ -n "$AAS_SERVER_HOST" ]; then
+  /app/wait-for-it.sh "$AAS_SERVER_HOST:${AAS_SERVER_PORT:-4001}"
+fi
+
+# Additional setup-time required before polling
+sleep 5
 
 # Run the main container command.
 exec "$@"
